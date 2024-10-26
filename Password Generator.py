1d3a19875405bd4ae0b60bb965cb4a40f475332e
Dhananjay <dhananjaysable370@gmail.com>
2024-10-27 00:29:20 +0530

    improved readability, efficiency, and flexibility

--- Password Generator.py
+++ Password Generator.py
@@ -1,19 +1,23 @@
 import random
+import string
 
-ps1 = "abcdefghijklmnopqrstuvwxyzABDCEFFGHIKLMNOPQRSTUVWXYZ0123456789!@#$%^&*."
-
-pass_length = int(input("Enter the length of password :"))
-
-pass_num = int(input("Enter the number of passwords you want :"))
+source = string.ascii_letters + string.digits + "!@#$%^&*"
 
 
-for i in range(0, pass_num):
-    password = ""
-    
-    for j in range(0,pass_length):
+def password_generator(length):
+    if length < 6:
+        print(
+            "Warning: Password length should be at least 6 characters for better security."
+        )
+        return ""
 
-        pass_char = random.choice(ps1)
-        password = password+pass_char
-    print("your password is: " +password )
+    return "".join(random.choice(source) for _ in range(length))
 
-    
+
+pass_length = int(input("Enter the length of each password: "))
+pass_num = int(input("Enter the number of passwords you want: "))
+
+for _ in range(pass_num):
+    password = password_generator(pass_length)
+    if password:
+        print("Your password is:", password)
